a492badcc682e94907e9b985304736e7dc1a6e6a
yasiru <yasirunet@gmail.com>
2020-10-31 00:07:00 +0530

    add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,16 +13,17 @@ module.exports = {
     },
     module:{
         rules: [
-            // {
-            //   test: /\.js$/,
-            //   exclude: /(node_modules)/,
-            //   use: {
-            //     loader: 'babel-loader',
-            //     options: {
-            //       presets: ['@babel/preset-env']
-            //     }
-            //   }
-            // },
+            {
+              test: /\.js$/,
+              exclude: /(node_modules)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env']
+                }
+              }
+            },
+            // un-comment this once you need a sass-loader
             // {
             //   test: /\.scss$/,
             //   use:[
